863f155de7ecb69fe432da87254c24c65de4f1cd
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-04-26 20:31:44 -0300

    third step completed :tada:

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,31 @@
-let canvas = document.querySelector('#gameCanvas');
+const canvas = document.querySelector("#gameCanvas");
 
-canvas.style.border = '1px solid #000';
+canvas.style.border = "1px solid #000";
+canvas.style.backgroundColor = "#F5F5F5";
+
+let snake = [
+  {
+    x: 150,
+    y: 150,
+  },
+
+  {
+    x: 140,
+    y: 150,
+  },
+
+  {
+    x: 130,
+    y: 150,
+  },
+
+  {
+    x: 120,
+    y: 150,
+  },
+
+  {
+    x: 110,
+    y: 150,
+  },
+];
